4f606626423b2dac82d2383d1c044863a5a8d643
Alex Albu <alexalbu78@gmail.com>
2024-12-03 10:24:11 +0200

    Tested web mvc layer using security config.
    Signed-off-by: Alex Albu <alexalbu78@gmail.com>

--- src/test/java/com/bookshop/catalog_service/web/BookControllerMvcTest.java
+++ src/test/java/com/bookshop/catalog_service/web/BookControllerMvcTest.java
@@ -1,24 +1,35 @@
 package com.bookshop.catalog_service.web;
 
+import com.bookshop.catalog_service.config.SecurityConfig;
 import com.bookshop.catalog_service.domain.BookNotFoundException;
 import com.bookshop.catalog_service.domain.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 class BookControllerMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private BookService bookService;
+
+    @MockBean
+    JwtDecoder jwtDecoder;
 
     @Test
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
@@ -29,5 +40,33 @@ class BookControllerMvcTests {
         mockMvc
                 .perform(get("/books/" + isbn))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()
+                                .authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(MockMvcRequestBuilders.delete("/books/" + isbn)
+                        .with(SecurityMockMvcRequestPostProcessors.jwt()
+                                .authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn401() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(MockMvcRequestBuilders.delete("/books/" + isbn))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
