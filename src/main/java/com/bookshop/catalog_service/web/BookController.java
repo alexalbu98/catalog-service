15d35dab81fe7e6f0ef5937028f96211f1aa1d93
Alex Albu <alexalbu78@gmail.com>
2024-12-04 10:42:25 +0200

    Added logging using slf4j.
    Signed-off-by: Alex Albu <alexalbu78@gmail.com>

--- src/main/java/com/bookshop/catalog_service/web/BookController.java
+++ src/main/java/com/bookshop/catalog_service/web/BookController.java
@@ -4,6 +4,8 @@ import com.bookshop.catalog_service.domain.Book;
 import com.bookshop.catalog_service.domain.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("books")
 @RequiredArgsConstructor
 public class BookController {
+    private static final Logger log =
+            LoggerFactory.getLogger(BookController.class);
     private final BookService bookService;
 
     @GetMapping
@@ -20,6 +24,9 @@ public class BookController {
 
     @GetMapping("{isbn}")
     public Book getByIsbn(@PathVariable String isbn) {
+        log.info(
+                "Fetching the list of books in the catalog"
+        );
         return bookService.viewBookDetails(isbn);
     }
 
